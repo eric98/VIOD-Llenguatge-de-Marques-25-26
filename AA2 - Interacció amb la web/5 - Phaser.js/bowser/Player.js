7729dd049717753ddbf30ef0d319df70f1ac15cb
eric <eric.garcia@enti.cat>
2026-04-17 12:37:44 +0200

    Inici Player.js

--- AA2 - Interacció amb la web/5 - Phaser.js/bowser/Player.js
+++ AA2 - Interacció amb la web/5 - Phaser.js/bowser/Player.js
@@ -5,8 +5,29 @@ export default class Player {
         this.create();
     }
 
-    create() {
-       
+    create(x,y) {
+        // 1. Configuració de físiques
+
+        // Afegim l'sprite
+        this.physicsSprite = this.scene.physics.add.sprite(x,y, 'dude');
+
+        // Activar el drag per al jugador
+        this.physicsSprite.setInteractive({ useHandCursor: true });
+        // Habilitar el sistema de draggable per a l'sprite
+        this.scene.input.setDraggable(this.physicsSprite);
+
+        // 2. Configuració d'animacions
+        this.scene.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        // Activar animació turn
+        // TODO
+
+        // 3. Configuració de la interacció de l'usuari
+
     }
 
     update() {

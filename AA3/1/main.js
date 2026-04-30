8bfbb02da07e1ac67f43c95c60801ddde829f59e
Eric Garcia Reverter <eric.garcia@enti.cat>
2026-04-30 09:55:42 +0200

    Afegit comentaris

--- AA3/1/main.js
+++ AA3/1/main.js
@@ -1,12 +1,20 @@
 import { db } from './firebase.js'
-import { collection, getDocs, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
 async function readScores() {
     const snapshot = await getDocs(collection(db, "puntuacions"));
 
+        // Recorrem els documents de la col·lecció "puntuacions"
         snapshot.forEach(doc => {
-        console.log(doc.data());
-    });
+
+            // Dades del document (nom, puntuacio, edat, data, etc.)
+            var dades = doc.data();
+
+            console.log(dades);
+        });
 }
 
 readScores();
+
+// Consultar a la base de dades de Firestore
+// https://firebase.google.com/docs/firestore/query-data/queries?hl=es-419#simple_queries
